9ef4a4f7bd9d766ab1dfb120f3bd768ca48961d0
MathiasCruz <mathiasaugustodacruz@gmaail.com>
2021-09-11 22:49:20 -0300

    Store v4 - Sequelizer, inclusão do gitigonore

--- NodeJs - Bootcamp/Modulo 3/Store-api/repositories/db.js
+++ NodeJs - Bootcamp/Modulo 3/Store-api/repositories/db.js
@@ -1,15 +1,11 @@
-import pg from 'pg';
+import Sequelize from 'sequelize';
 
-async function connect() {
-  if (global.connection) {
-    return global.connection.connect();
+const sequelize = new Sequelize(
+  'postgres://izzrgthd:7hBogXLmz4RGcdlREgI2oL5y-S06VWgV@kesavan.db.elephantsql.com/izzrgthd',
+  {
+    dialect: 'postgres',
+    define: { timestamp: false },
   }
-  const pool = new pg.Pool({
-    connectionString:
-      'postgres://izzrgthd:7hBogXLmz4RGcdlREgI2oL5y-S06VWgV@kesavan.db.elephantsql.com/izzrgthd',
-  });
-  global.connection = pool;
-  return pool.connect();
-}
+);
 
-export { connect };
+export default sequelize;
