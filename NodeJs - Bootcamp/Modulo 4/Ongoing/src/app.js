9575cc8631182d3d21539638e6880f77f44308a4
MathiasCruz <mathiasaugustodacruz@gmail.com>
2021-09-26 18:45:30 -0700

    TDD testes de integração v2

--- NodeJs - Bootcamp/Modulo 4/Ongoing/src/app.js
+++ NodeJs - Bootcamp/Modulo 4/Ongoing/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 
+const app = express()
+const db = require('./db.js')
 app.get('/', async (req, res) => {
   res.status(200).send('Bootcamp desenvolvedor backend - topicos especiais')
 })
--- NodeJs - Bootcamp/Modulo 4/Ongoing/src/server.js
+++ NodeJs - Bootcamp/Modulo 4/Ongoing/src/server.js
@@ -1,5 +1,11 @@
 const app = require('./app')
+const db = require('./db.js')
+
+db.sequelize.sync().then(async ()=>{
+  await console.log('Conectado ao banco de dados')
+})
 
 app.listen(5678, () => {
   console.log('Aplicação rodando na porta 5678')
 })
+//ec3eed499b20 docker container
